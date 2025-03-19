53ef59cecd8833e8f552cd0af40eed5136446cfe
viniciusfmorais <viniciusfmorais@hotmail.com>
2025-03-19 06:20:27 -0400

    Implemented dealer fetch endpoints

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
     "localhost",
-    "127.0.0.1",
+    "127.0.0.1"
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://127.0.0.1:8000"
+    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
 ]
 
 REST_FRAMEWORK = {

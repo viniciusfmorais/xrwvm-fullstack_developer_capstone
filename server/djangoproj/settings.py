f1d8a977604ca7469a58b54ff26d86c97cb0b6de
viniciusfmorais <viniciusfmorais@hotmail.com>
2025-04-07 05:17:39 -0400

    Fixed

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://viniciusfmor-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
     "localhost",
     "127.0.0.1"
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://viniciusfmor-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
 ]
 
 REST_FRAMEWORK = {

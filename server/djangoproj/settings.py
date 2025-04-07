7f1bde04d1aec3d4961eb63f4d6bfdf37d83fe3f
Vinicius Ferro de Morais <viniciusfmorais@hotmail.com>
2025-04-07 14:13:00 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,18 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
+SECRET_KEY =
+"django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-"
+    "theiak8s-4-tor01.proxy.cognitiveclass.ai",
     "localhost",
     "127.0.0.1"
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    "https://viniciusfmor-8000.theiadockernext-1-labs-prod-"
+    "theiak8s-4-tor01.proxy.cognitiveclass.ai"
 ]
 
 REST_FRAMEWORK = {
